5b26fa1284ee62dab8399ae43f1eab2b6fde81c3
Rohit Agrawal <110839621+rohitagr0310@users.noreply.github.com>
2023-04-13 21:25:21 +0530

    Villager Random Movement Done

---
+++ Emberfire/Assets/Scripts/Interactable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public bool playerInRange;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInRange = false;
+        }
+    }
+}
